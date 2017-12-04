a45993da4e831bbf0843140bb72d23246943d3fd
Mindaugas Barčauskas <mindaugas.barcauskas@gmail.com>
2017-12-04 13:43:20 +0200

    first commit

---
+++ src/main/java/com/mindaugasbar/memployeemanagement/MemployeemanagementApplication.java
@@ -0,0 +1,12 @@
+package com.mindaugasbar.memployeemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemployeemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemployeemanagementApplication.class, args);
+	}
+}
